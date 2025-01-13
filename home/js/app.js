c5ac3fd16c1593304eab4f37093734d65c038d2e
apccmfamador <cmfamador@student.apc.edu.ph>
2025-01-13 22:44:38 +0800

    deleted guestbook.js

--- home/js/app.js
+++ home/js/app.js
@@ -8,3 +8,36 @@ new Vue({
         ]
     }
 });
+
+new Vue({
+    el: '#app',
+    data: {
+        images: [
+            { src: 'images/image1.jpg', alt: 'Image 1' },
+            { src: 'images/image2.jpg', alt: 'Image 2' },
+            { src: 'images/image3.jpg', alt: 'Image 3' }
+        ],
+        newEntry: {
+            name: '',
+            message: '',
+            timestamp: ''
+        },
+        entries: [] // Now correctly defined within the Vue instance
+    },
+    mounted() {
+        if (localStorage.getItem('guestbookEntries')) {
+            this.entries = JSON.parse(localStorage.getItem('guestbookEntries'));
+        }
+    },
+    methods: {
+        addEntry() {
+            if (this.newEntry.name && this.newEntry.message) {
+                this.newEntry.timestamp = new Date().toLocaleString();
+                this.entries.push({ ...this.newEntry });
+                localStorage.setItem('guestbookEntries', JSON.stringify(this.entries));
+                this.newEntry.name = '';
+                this.newEntry.message = '';
+            }
+        }
+    }
+});

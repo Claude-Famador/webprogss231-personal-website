6b4ffe8bfa59302649c61be963326ad8161ddca7
apccmfamador <cmfamador@student.apc.edu.ph>
2025-01-13 16:40:20 +0800

    initial commit

---
+++ home/js/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        images: [
+            { src: 'images/htmlimg1.jpg', alt: 'Research group with consultant' },
+            { src: 'images/htmlimg2.jpg', alt: 'After the Defense' }
+            // Add more images here
+        ]
+    }
+});
